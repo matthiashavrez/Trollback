4a3083fa4dd08b27b66fcb0eff15d40f91e8c965
Julien Thirion <jln.thirion@gmail.com>
2013-07-01 02:05:10 +0200

    Maquette Up
    Ajout des scripts JS sur les blocs évenements, news et colonne de droite
    Gestion de la résolution
    
    A faire :
    - Ajustement visuels sur les colonnes de droite
    - Plus de dynamisme sur la maquette
    - Gestion des blocs en résolution inférieure à 1280
    - Police en font-face
    - Créer le login "User connecté"

--- maquette/script.js
+++ maquette/script.js
@@ -1,6 +1,50 @@
 var dimension = DetectDimension();
 
 $(document).ready(function(){
+	// Quand on clique sur un élément liste du bloc news
+	$('div.page>div.contentLeft>div.bloc_news>div.texte>div.listeNews>ul>li>h2').live('click',function() {
+		var classe = $(this).attr("class");
+		// On change la classe active du li
+		$('div.page>div.contentLeft>div.bloc_news>div.texte>div.listeNews>ul>li').removeClass('active');
+		$(this).parent().addClass('active');
+
+		// On fadeOut le lecteur courant
+		$('div.page>div.contentLeft>div.bloc_news>div.texte>div.lecteurNews.active').fadeOut(500,function() {
+			$(this).removeClass('active');
+			$('div.page>div.contentLeft>div.bloc_news>div.texte>div.lecteurNews.'+classe).fadeIn(500,function() { $(this).addClass('active'); })
+		});
+	});
+
+	// Quand on clique sur un élément liste du bloc event
+	$('div.page>div.contentLeft>div.bloc_events>div.texte>div.listeEvents>ul>li>h2').live('click',function() {
+		var classe = $(this).attr("class");
+		// On change la classe active du li
+		$('div.page>div.contentLeft>div.bloc_events>div.texte>div.listeEvents>ul>li').removeClass('active');
+		$(this).parent().addClass('active');
+
+		// On fadeOut le lecteur courant
+		$('div.page>div.contentLeft>div.bloc_events>div.texte>div.lecteurEvents.active').fadeOut(500,function() {
+			$(this).removeClass('active');
+			$('div.page>div.contentLeft>div.bloc_events>div.texte>div.lecteurEvents.'+classe).fadeIn(500,function() { $(this).addClass('active'); })
+		});
+	});
+
+	// Quand on clique sur un élément liste du bloc event
+	$('div.page>div.contentRight>div.colonneSelection>div.item').live('click',function() {
+		var classe = $(this).attr("id");
+		// On change la classe active du li
+		$('div.page>div.contentRight>div.colonneSelection>div.item').removeClass('active');
+		$(this).addClass('active');
+
+		// On fadeOut le lecteur courant
+		$('div.page>div.contentRight>div.colonneProjet.active').fadeOut(500,function() {
+			$(this).removeClass('active');
+			$('div.page>div.contentRight>div.colonneProjet.'+classe).css({'height':(dimension[1]-100)+'px'}).fadeIn(500,function() { $(this).addClass('active'); })
+		});
+	});
+
+	$('div.page>div.contentRight>div.colonneProjet.active').css('height',(dimension[1]-100)+'px');
+
 	redim();
 });
 
